6f2f0903cf470ee9af29082077126a4f996b018b
dankernel <google@dankook.ac.kr>
2014-11-25 03:01:35 +0900

    auto-20141125-030135

---
+++ test/333/main.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 11월 25일 03시 01분 26초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+  printf("333\n");
+
+  return 0;
+}
+
